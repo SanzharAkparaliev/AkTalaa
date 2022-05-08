0928f43ba37f5e038da3f209c8ffd216a186e98e
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-05-08 13:46:13 +0600

    first commit

---
+++ src/main/java/com/example/taxiaktalaa/TaxiAkTalaaApplication.java
@@ -0,0 +1,13 @@
+package com.example.taxiaktalaa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaxiAkTalaaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaxiAkTalaaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/taxiaktalaa/TaxiAkTalaaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taxiaktalaa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaxiAkTalaaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
